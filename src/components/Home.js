f452b7f4476503cef30751a27eb52f8f87d76274
lerderl <josepholukunle1107@gmail.com>
2022-08-23 12:54:05 +0100

    Add Customer Suppport section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import Header from "./Header";
+import CustomerSupport from "./sections/CustomerSupport";
 
 const Home = () => {
   return (
     <div id="main-wrapper">
       <Header />
+
+      {/* <!-- Content
+      ============================================= --> */}
+      <div id="content">
+        <CustomerSupport />
+      </div>
+      {/* <!-- Content end -->  */}
     </div>
   );
 };
--- src/components/sections/CustomerSupport.js
+++ src/components/sections/CustomerSupport.js
@@ -1,3 +1,18 @@
-const CustomerSupport = () => {};
+import { Link } from "react-router-dom";
+
+const CustomerSupport = () => {
+  return (
+    <section className="hero-wrap section shadow-md">
+      <div className="hero-mask opacity-9 bg-success"></div>
+      <div className="hero-bg customer-support-image"></div>
+      <div className="hero-content py-5">
+        <div className="container text-center">
+          <h2 className="text-9 text-white">Awesome Customer Support</h2>
+          <p className="lead text-white mb-4">Have you any query? Don't worry. We have great people ready to help you whenever you need it.</p>
+          <Link to="/help" className="btn btn-light">Find out more</Link> </div>
+      </div>
+    </section>
+  );
+};
 
 export default CustomerSupport;
